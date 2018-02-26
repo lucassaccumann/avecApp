da54df52c8f104a83c8d426ce967f80a127da18b
Lucas <lucas.sacuman@gmail.com>
2018-02-25 21:51:15 -0300

    AvecApp 1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -18,7 +17,6 @@ import { FotosProvider } from '../providers/fotos/fotos';
   declarations: [
     MyApp,
     HomePage,
-    ListPage,
   ],
   imports: [
     BrowserModule,
@@ -31,7 +29,6 @@ import { FotosProvider } from '../providers/fotos/fotos';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage,
   ],
   providers: [
     StatusBar,
--- src/providers/fotos/fotos.ts
+++ src/providers/fotos/fotos.ts
@@ -1,12 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the FotosProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class FotosProvider {
   private baseApiPath = "http://api.salaovip.com.br";
